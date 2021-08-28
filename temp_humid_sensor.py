7715c50954d7af02e0ee0e7ff537865ca7c9cae4
FRAZ5094 <frasergmorrison@btinternet.com>
2021-08-28 14:58:57 +0100

    added temperate and humidity sensor

---
+++ temp_humid_sensor.py
@@ -0,0 +1,19 @@
+import Adafruit_DHT
+import time
+
+DHT_SENSOR=Adafruit_DHT.DHT11
+DHT_PIN=4
+
+max_attempts=10
+attempt=0
+
+temperature=None
+
+while attempt<max_attempts and temperature==None:
+    humidity,temperature= Adafruit_DHT.read(DHT_SENSOR,DHT_PIN)
+    attempt+=1
+
+if attempt==max_attempts:
+    print("Sensor error")
+else:
+    print(f"{int(time.time())},{temperature},{humidity}")
