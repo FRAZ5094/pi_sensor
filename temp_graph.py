49ddb5fd07d41c1e2de5b80bcf36673678debf3a
FRAZ5094 <frasergmorrison@btinternet.com>
2021-08-29 20:41:26 +0100

    graphs changed

--- temp_graph.py
+++ temp_graph.py
@@ -3,6 +3,7 @@ import sys
 from itertools import islice
 
 import matplotlib.pyplot as plt
+import pandas as pd
 
 filename = sys.argv[1]
 
@@ -17,13 +18,24 @@ with open(filename, "r") as f:
             times.append(datetime.datetime.fromtimestamp(int(time)))
             temps.append(int(temp))
             humids.append(int(humid))
+data = pd.DataFrame({"time": times, "temp": temps, "humidity": humids})
+data["MA_temp_30"] = data["temp"].rolling(30, center=True).mean()
+data["MA_humid_30"] = data["humidity"].rolling(30, center=True).mean()
 plt.figure(1)
-plt.plot(times, temps)
+plt.plot(data["time"], data["temp"], label="Temperature")
+plt.plot(data["time"], data["MA_temp_30"], label="Temperature (30min rolling average)")
+plt.legend()
 plt.title("Temperature over time")
-# plt.ylim(0, 50)
+plt.xlabel("Time")
+plt.ylabel(f"Temperature ({chr(176)}C)")
+plt.ylim(15, 30)
 plt.grid()
 plt.figure(2)
-plt.plot(times, humids)
+plt.plot(data["time"], data["humidity"], label="Humidity")
+plt.plot(data["time"], data["MA_humid_30"], label="Humidity (30min rolling average)")
+plt.legend()
 plt.title("Humidity over time")
+plt.xlabel("Time")
+plt.ylabel("Humidity (%)")
 plt.grid()
 plt.show()
