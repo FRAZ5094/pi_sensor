a1ffa5716119838b0d44de85dfc70bb1a3e6fc4d
FRAZ5094 <frasergmorrison@btinternet.com>
2021-08-29 18:42:31 +0100

    fixed temp and humid graph

--- temp_graph.py
+++ temp_graph.py
@@ -1,3 +1,4 @@
+import datetime
 import sys
 from itertools import islice
 
@@ -13,10 +14,16 @@ with open(filename, "r") as f:
         cols = line.split(",")
         if len(cols) > 1:
             time, temp, humid = cols
-            times.append(int(time))
+            times.append(datetime.datetime.fromtimestamp(int(time)))
             temps.append(int(temp))
             humids.append(int(humid))
-
+plt.figure(1)
 plt.plot(times, temps)
-plt.ylim(0, 50)
+plt.title("Temperature over time")
+# plt.ylim(0, 50)
+plt.grid()
+plt.figure(2)
+plt.plot(times, humids)
+plt.title("Humidity over time")
+plt.grid()
 plt.show()
